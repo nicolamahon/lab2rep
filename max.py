16633b91d1e87a97b0cb4d4ba2cafe0a877d85d2
Nicola Mahon <c15755031@mydit.ie>
2017-09-27 13:43:20 +0000

    python program to find max value in an array

---
+++ max.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+list = [123, 89, 4567, 0.1]
+
+print "List contains: "
+for i in list:
+	print i
+print "Max value element : ", max(list)
+print "Min value element : ", min(list)
